4dd697ebb5f3e977a509c21312fb3f250948d8fe
Jun Yokoyama <jun@larus.org>
2015-07-09 16:49:14 +0900

    added -l option (simple formatted list)

--- lib/ec2list/cli.rb
+++ lib/ec2list/cli.rb
@@ -9,6 +9,7 @@ module Ec2list
     option :k, type: :string
     option :s, type: :string
     option :t, type: :string
+    option :l, type: :boolean, default: false
     
     desc "display", "display instances list"
     def display
@@ -16,12 +17,20 @@ module Ec2list
         ec2_list.values(options[:k].to_sym).each do |v|
           puts v if v.length > 0
         end
+      elsif options[:l]
+        ec2_list.result.each do |instance|
+          puts display_columns.map { |column| instance[column] }.join(' ')
+        end
       else
-        Formatador.display_compact_table ec2_list.result, [:id, :type, :tag, :status, :fqdn, :ip_addr, :since]
+        Formatador.display_compact_table ec2_list.result, display_columns
       end
     end
 
     private
+
+    def display_columns
+      [:id, :type, :tag, :status, :fqdn, :ip_addr, :since]
+    end
 
     def ec2_list
       @ec2_list ||= Ec2list::Instances.new(option_strings)
